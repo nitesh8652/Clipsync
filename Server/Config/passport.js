ba1a9cc03f48a7ec82f0cd8e7ac6ea825e98720a
nitesh8652 <niteshsalian8652085233@gmail.com>
2026-05-06 00:16:15 +0530

    final

--- Server/Config/passport.js
+++ Server/Config/passport.js
@@ -7,7 +7,9 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: "/auth/google/callback"
+            callbackURL: process.env.NODE_ENV === "production"
+            ? "https://clipsync-gedv.onrender.com/auth/google/callback"
+            : "http://localhost:5000/auth/google/callback"
         },
         async (accessToken, refreshToken, profile, done) => {
             try {
